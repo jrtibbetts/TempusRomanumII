67d3198fe78243799b448e37639a47236221e1fb
Jason R Tibbetts <jason@tibbetts.net>
2019-06-08 06:38:28 -0400

    Copied over the color assets from TempusRomanum and gave a gradient to the ClockFace.

---
+++ TempusRomanum/ClockFace.swift
@@ -0,0 +1,28 @@
+//  Copyright © 2019 Poikile Creations. All rights reserved.
+
+import SwiftUI
+
+struct ClockFace : View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let frame = CGRect(x: 0.0, y: 0.0, width: width, height: width)
+                
+                path.addEllipse(in: frame)
+            }
+                .fill(LinearGradient(gradient: .init(colors: [Color("Daylight"), Color("Nighttime")]), startPoint: .init(x: 0.5, y: 0.0), endPoint: .init(x: 0.5, y: 0.5)))
+        }
+    }
+    
+    static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+    static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
+}
+
+#if DEBUG
+struct ClockFace_Previews : PreviewProvider {
+    static var previews: some View {
+        ClockFace()
+    }
+}
+#endif
