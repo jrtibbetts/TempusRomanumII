be742ac7bd8d40aeae1582189a4ef3d501ac770e
Jason R Tibbetts <jason@tibbetts.net>
2019-06-12 20:06:43 -0400

    Fixed the math in Date+Trigonometry.

--- TempusRomanum/SunriseSunset/Date+Trigonometry.swift
+++ TempusRomanum/SunriseSunset/Date+Trigonometry.swift
@@ -6,28 +6,25 @@ import SwiftUI
 internal extension Date {
 
     var minutesAngle: Angle {
-        let midnight = Calendar.current.startOfDay(for: self)
-        let timeInMinutes = timeIntervalSince(midnight) / 60
-        let percentage = timeInMinutes / 12.0 * 60.0 * 60.0
-        
-        return Angle(radians: (Double.pi / 2.0) + (Double.pi * 2 * Double(percentage)))
+        return angle(startingAt: Angle(radians: (Double.pi * 1.5)), divisions: 60.0 * 60.0)
     }
 
     var hour12RotationAngle: Angle {
-        let midnight = Calendar.current.startOfDay(for: self)
-        let timeInMinutes = timeIntervalSince(midnight) / 60
-        let percentage = timeInMinutes / 12.0 * 60.0
-        
-        return Angle(radians: (Double.pi / 2.0) + (Double.pi * 2 * Double(percentage)))
+        return angle(startingAt: Angle(radians: (Double.pi * 0.5)), divisions: 12.0 * 60.0 * 60.0)
     }
     
     /// The angle, in radians, of the `Date`, relative to 12:00 am the same day.
     var hour24RotationAngle: Angle {
+        return angle(startingAt: Angle(radians: (Double.pi * 0.5)), divisions: 24.0 * 60.0 * 60.0)
+    }
+    
+    func angle(startingAt startingPoint: Angle,
+               divisions: Double) -> Angle {
         let midnight = Calendar.current.startOfDay(for: self)
-        let timeInMinutes = timeIntervalSince(midnight) / 60
-        let percentage = timeInMinutes / 24.0 * 60.0
+        let timeInMinutes = timeIntervalSince(midnight).truncatingRemainder(dividingBy: divisions)
+        let percentage = timeInMinutes / (divisions)
         
-        return Angle(radians: (Double.pi / 2.0) + (Double.pi * 2 * Double(percentage)))
+        return Angle(radians: startingPoint.radians + (Double.pi * 2 * Double(percentage)))
     }
     
 }
